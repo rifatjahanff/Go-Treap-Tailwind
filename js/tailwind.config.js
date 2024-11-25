f69ea3b108c900bcd15a224e4fac17867567b533
rifatjahanff <jahanrifat953@gmail.com>
2024-11-25 07:17:21 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,28 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          button: '#F8D448',
+          fontcolor: '#697488',
+          success: '#E0FFF9',
+          btn:'#F5F5F5',
+          footer:`#3554D1`
+        },
+
+        maxWidth: {
+          container: '1320px',
+        },
+        backgroundImage: {
+          
+          bannerImage: "url('./images/aaaa.jpg')",
+
+          footerImage: "url('./images/testimonials.png')",
+        },
+
+        
+
+      }
+    }
+  }
